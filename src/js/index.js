73e0d674f301a6c088b488f0f6fa9550bc91737d
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2022-11-03 21:30:49 +0200

    make desc open window

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,17 @@
 const women = document.querySelector('.women');
 const men = document.querySelector('.men');
 const list = document.querySelectorAll('.menu__list-name');
-
+const menuList = document.querySelector('.desc__menu');
+console.log(menuList);
 // const span = document.querySelector('span')
-console.log(men, women, list);
+// console.log(men, women, list);
 women.addEventListener('click', function (e) {
   e.preventDefault();
   women.style.color = '#0F303F';
   men.style.color = '#B7C1C5';
   women.classList.add('line-color');
   men.classList.remove('line-color');
+  menuList.style.display = 'block';
 });
 men.addEventListener('click', function (e) {
   e.preventDefault();
@@ -17,6 +19,7 @@ men.addEventListener('click', function (e) {
   men.style.color = '#0F303F';
   men.classList.add('line-color');
   women.classList.remove('line-color');
+  menuList.style.display = 'block';
 });
 
 list.forEach(n => {
