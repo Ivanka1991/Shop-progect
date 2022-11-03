4da1ef603b25b243d5a60ada90e583e69e6cc5b1
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2022-11-03 18:35:38 +0200

    finish with mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
+const women = document.querySelector('.women');
+const men = document.querySelector('.men');
+const list = document.querySelectorAll('.menu__list-name');
 
+// const span = document.querySelector('span')
+console.log(men, women, list);
+women.addEventListener('click', function (e) {
+  e.preventDefault();
+  women.style.color = '#0F303F';
+  men.style.color = '#B7C1C5';
+  women.classList.add('line-color');
+  men.classList.remove('line-color');
+});
+men.addEventListener('click', function (e) {
+  e.preventDefault();
+  women.style.color = '#B7C1C5';
+  men.style.color = '#0F303F';
+  men.classList.add('line-color');
+  women.classList.remove('line-color');
+});
 
-
+list.forEach(n => {
+  n.addEventListener('click', e => {
+    const clicked = e.target;
+    clicked.parentElement.lastElementChild.classList.toggle('open');
+  });
+});
